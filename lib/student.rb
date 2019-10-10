71443797a4e12d6bff94bd488b7176b1f14540e0
Doug Kintop <dkintop@hotmail.com>
2019-10-10 15:32:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,6 +90,14 @@ class Student
     end.first(x) 
   end
   
-  
+  def self.first_student_in_grade_10
+    sql = <<-SQL 
+    SELECT * FROM students 
+    WHERE students.grade = 10
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first 
+  end
   
 end
