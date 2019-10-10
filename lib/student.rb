f8aa2ab619813cdee63d842f60f1cd42e47fd059
Doug Kintop <dkintop@hotmail.com>
2019-10-10 15:28:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,7 @@ class Student
   
   
   
-   def self.first_x_students_in_grade_10(x)
+   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL 
     SELECT * FROM students 
     WHERE grade = 10
