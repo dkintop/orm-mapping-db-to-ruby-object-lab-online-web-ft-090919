c68854885151bea626da76839ff788349ffc28f5
Doug Kintop <dkintop@hotmail.com>
2019-10-10 15:35:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,7 +100,7 @@ class Student
     end.first 
   end
   
-   def self.all_student_in_grade_X(x)
+   def self.all_students_in_grade_X(x)
     sql = <<-SQL 
     SELECT * FROM students 
     WHERE students.grade = ?
