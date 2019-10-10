9aff2ac4379d80e128d1005853674734254744f2
Doug Kintop <dkintop@hotmail.com>
2019-10-10 15:00:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,9 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
    sql = <<-SQL 
-   SELECT * FROM students
+   SELECT * FROM students;
+   SQL 
+   DB[:conn].execute(sql)
    
   end
   
